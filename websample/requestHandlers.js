4abf2249b69146c49154183647068c84a92c59bd
Rock <a0988042190@gmail.com>
2020-05-08 01:38:32 +0800

    first

--- websample/requestHandlers.js
+++ websample/requestHandlers.js
@@ -21,18 +21,18 @@ function upload(response, exec, postData) {
   exec('touch /etc/httpd/conf.d/' + querystring.parse(postData).Document + '.conf', function (err, stdout, stderr) {
     console.log('touch : ');
     console.log(stdout);
-    exec('echo -e "<VirtualHost *:80>' +
-      ' ServerName ' + querystring.parse(postData).Url +
-      ' DocumentRoot  /var/www/html/' + querystring.parse(postData).Document +
-      ' ErrorLog logs/' + querystring.parse(postData).Document +
-      ' CustomLog logs/' + querystring.parse(postData).Document + '_log common' +
-      ' <Directory "/var/www/html/' + querystring.parse(postData).Document + '">' +
-      ' Options FollowSymLinks' +
-      ' AllowOverride None' +
-      ' Order allow,deny' +
-      ' allow from all' +
-      ' </Directory>' +
-      ' </VirtualHost>" > /etc/httpd/conf.d/' + querystring.parse(postData).Document + '.conf;', function (err, stdout, stderr) {
+    exec('echo "<VirtualHost *:80>"' +
+         'echo "ServerName ' + querystring.parse(postData).Url +'"'+
+         'echo "DocumentRoot  /var/www/html/' + querystring.parse(postData).Document +'"'+
+         'echo "ErrorLog logs/' + querystring.parse(postData).Document +'"'+
+         'echo "CustomLog logs/' + querystring.parse(postData).Document + '_log common"' +
+         'echo "<Directory "/var/www/html/' + querystring.parse(postData).Document + '">"' +
+         'echo "Options FollowSymLinks"' +
+         'echo "AllowOverride None"' +
+         'echo "Order allow,deny"' +
+         'echo "allow from all"' +
+         'echo "</Directory>"' +
+         'echo "</VirtualHost>" > /etc/httpd/conf.d/' + querystring.parse(postData).Document + '.conf;', function (err, stdout, stderr) {
         console.log('-e : ');
         console.log(err);
         // response.write(stdout);
