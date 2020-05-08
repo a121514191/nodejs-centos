9981c2a85b64e672740f3da7fbc8372498d0ea6c
Rock <a0988042190@gmail.com>
2020-05-08 13:28:25 +0800

    database

--- websample/requestHandlers.js
+++ websample/requestHandlers.js
@@ -10,7 +10,7 @@ function start(response, exec, postData, spawn) {
   response.end();
 }
 
-function upload2(response, exec, postData) {
+function upload(response, exec, postData) {
   console.log("Request handler 'upload' was called.");
   // response.writeHead(200, { "Content-Type": "text/plain" });
   // response.write("You're Url is : " +
@@ -47,7 +47,7 @@ function upload2(response, exec, postData) {
 }
 
 
-function upload(response, exec, postData, spawn) {
+function upload2(response, exec, postData, spawn) {
   console.log("asciinema test start");
   const asci_start = spawn('asciinema ', ['rec']);
   // const asci_ls = spawn('ls', ['-la']);
@@ -72,10 +72,10 @@ function upload(response, exec, postData, spawn) {
   //     console.log("成功");
   //   }
   // });
-  var body = '<!doctype html> <html lang="en"> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"> </head> <body> <div class="container"> <div class="row"> <div class="col-2"> </div> <div class="col-8" style="text-align: center;"> <script id="asciicast-psk6wrIMsENOJB5Ew2UqLUA7V" data-autoplay="true" src="https://asciinema.org/a/psk6wrIMsENOJB5Ew2UqLUA7V.js" async></script> </div> <div class="col-2"> </div> </div> <!-- Optional JavaScript --> <!-- jQuery first, then Popper.js, then Bootstrap JS --> <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> </body> </html>';
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(body);
-  response.end();
+  // var body = '<!doctype html> <html lang="en"> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"> </head> <body> <div class="container"> <div class="row"> <div class="col-2"> </div> <div class="col-8" style="text-align: center;"> <script id="asciicast-psk6wrIMsENOJB5Ew2UqLUA7V" data-autoplay="true" src="https://asciinema.org/a/psk6wrIMsENOJB5Ew2UqLUA7V.js" async></script> </div> <div class="col-2"> </div> </div> <!-- Optional JavaScript --> <!-- jQuery first, then Popper.js, then Bootstrap JS --> <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> </body> </html>';
+  // response.writeHead(200, { "Content-Type": "text/html" });
+  // response.write(body);
+  // response.end();
 }
 
 exports.start = start;
