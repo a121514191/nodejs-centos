8d99870c55cec4066c439c2b8c0c445633cda841
Rock <a0988042190@gmail.com>
2020-05-15 00:03:27 +0800

    form

--- websample/requestHandlers.js
+++ websample/requestHandlers.js
@@ -60,20 +60,20 @@ function upload(response, exec, request) {
   // response.end();
 }
 
-// function show(response, exec, postData) {
-//   console.log("Request handler 'show' was called.");
-//   fs.readFile("/tmp/test.png", "binary", function(error, file) {
-//     if(error) {
-//       response.writeHead(500, {"Content-Type": "text/plain"});
-//       response.write(error + "\n");
-//       response.end();
-//     } else {
-//       response.writeHead(200, {"Content-Type": "image/png"});
-//       response.write(file, "binary");
-//       response.end();
-//     }
-//   });
-// }
+function show(response, exec, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
 
 //理想函式
 function upload_want(response, exec, postData) {
